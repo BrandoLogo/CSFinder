d45794eddca75789bb69e39e70305ce112386ae0
Jefferson <95506163+RedrixKell@users.noreply.github.com>
2023-09-27 10:30:35 -1000

    Add files via upload

---
+++ signUp.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title></title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div class = "container"> 
+	<h1> Sign Up </h1>
+	<p>Please fill out this page to create your account</p>
+	<hr>
+
+	  <label for="email"><b>Email</b></label>
+	  <input type = "email" placeholder="Enter Email" name="email" required></input>
+
+	<label for = "password"><b>Password</b?></label>
+	  <input type = "password" placeholder = "Enter Password" name = "password" required></input>
+
+	 <label>
+	      <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+	    </label>
+
+	<div class ="clearFix">
+	  <button type = "button" class = "cancelbtn">Cancel</button>
+	  <button type = "button" class = "signupbtn">Sign Up</button>
+	</div>
+</div>
+</body>
+
+</html>
